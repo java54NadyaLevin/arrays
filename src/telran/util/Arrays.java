2a299be3e4f1df5db2e877eba5b38f336ce89b72
Nadelele <nadele@gmail.com>
2024-03-28 19:25:10 +0200

    HW11 todo

--- src/telran/util/Arrays.java
+++ src/telran/util/Arrays.java
@@ -30,15 +30,37 @@ public class Arrays {
 		return res;
 	}
 
+//HomeWork
 	public static <T> void bubbleSort(T[] array, Comparator<T> comp) {
-		for (int j = 0; j < array.length; j++) {
+		boolean isSorted = true;
+		for (int j = 0; j < array.length - 1 && isSorted; j++) {
+			isSorted = false;
 			for (int i = 0; i < array.length - j - 1; i++) {
 				if (comp.compare(array[i], array[i + 1]) > 0) {
 					T tmp = array[i];
 					array[i] = array[i + 1];
 					array[i + 1] = tmp;
+					isSorted = true;
 				}
 			}
 		}
 	}
+	
+	public static <T> int binarySearch(T[] array, T key, Comparator<T> comp) {
+		//TODO
+		//only sorted array!
+		//if key == length/2  -  return length/2??
+		//if key < length/2 - search again in first half. else - in second half
+		//left index = 0;
+		//right index = array.length-1;
+		//middle index = (left + right) /2;
+		//left part: left index; right  = middle-1;
+		//right part: left index = middle + 1; right index;
+		//while  left <= right - additional statement;
+		//returns as binary search;
+		//if there are several equal elements, 
+		//any of indexes can be returned - no guarantee
+		return -1;
+	}
+
 }
